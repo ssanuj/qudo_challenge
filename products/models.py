a5d3f2c71ec93875ba350392f985fa2289ac11c7
Serena <serena.kuriakose@gmail.com>
2023-01-18 17:12:52 +0000

    updated the order model

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -20,3 +21,12 @@ class Product(models.Model):
             raise ValidationError('Out of Stock')
     class Meta:
         db_table = "Product"
+
+class Order(models.Model):
+    order_name = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %Y %I:%M %p")}'
