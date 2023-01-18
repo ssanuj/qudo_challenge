568ca6f75824ff66a566f0783ff57c599f1b92ec
Serena <serena.kuriakose@gmail.com>
2023-01-18 17:14:01 +0000

    Added the admin registration for the order model

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product  # import your model
+from .models import Product, Order  # import your models
 
 # Register your models here.
 
 admin.site.register(Product) # actual registration
+admin.site.register(Order) # actual registration
