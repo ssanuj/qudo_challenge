25f93cf027fcbb4a06c1d5434d4cca5cf6cd8352
Serena <serena.kuriakose@gmail.com>
2023-01-18 17:11:15 +0000

    updated the url patterns

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+
 urlpatterns = [
-    
+
     path('',views.products, name='products'),
+    path('orders',views.orders,name='orders'),
+    path('products/place_order', views.place_order, name ='place_order'),
    
 ]
     
